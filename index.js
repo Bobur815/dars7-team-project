6e68cb389c3d958c702a964118e80255395ed6fd
Bobur815 <ergashevboburmirzo815@gmail.com>
2025-08-06 15:32:42 +0500

    b

---
+++ index.js
@@ -0,0 +1,41 @@
+
+const viewContainer = document.getElementById('app-view');
+const homeIcon = document.getElementById('home-icon')
+
+homeIcon.addEventListener('click', (e) => {
+    e.preventDefault()
+    loadView('./home.html');
+})
+
+async function loadView(url) {
+    try {
+        const res = await fetch(url);
+        if (!res.ok) throw new Error(`Failed to load ${url}`);
+        const html = await res.text();
+        viewContainer.innerHTML = html;
+    } catch (err) {
+        viewContainer.innerHTML = `<p style="color:red">Error loading page.</p>`;
+        console.error(err);
+    }
+}
+
+document.querySelectorAll('a[data-view]').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelectorAll('a[data-view]').forEach(l => {
+            l.classList.remove('active');
+        });
+
+        link.classList.add('active');
+
+        const url = link.getAttribute('href');
+        loadView(url);
+        history.pushState(null, '', url);
+    });
+});
+
+window.addEventListener('popstate', () => {
+    loadView(location.pathname);
+});
+
+loadView('./home.html');
